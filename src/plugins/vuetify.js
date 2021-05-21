b7bcc23c51f006a3724fbc881b91aa883e330231
jonghyun <jonghyeon2046@gmail.com>
2021-05-21 18:29:20 +0900

    [UPDATE]toast 설정

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,42 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib/framework'
+import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib'
 
-Vue.use(Vuetify)
+import en from 'vuetify/es5/locale/en'
+import ko from 'vuetify/es5/locale/ko'
 
-export default new Vuetify({
+Vue.use(Vuetify, {
+  components: {
+    VSnackbar,
+    VBtn,
+    VIcon
+  }
 })
+const theme = {
+  themes: {
+    light: {
+      primary: '#344059',
+      secondary: '#BFBFBF', // #FFCDD2
+      accent: '#2C4027',
+      info: '#4E7DA6',
+      success: '#26A699',
+      warning: '#F29727',
+      error: '#F24C3D'
+
+      // success: '#2C4027'
+    }
+    // dark: {
+    //   primary: colors.brown.darken1, // #E53935
+    //   secondary: colors.deepPurple.lighten4, // #FFCDD2
+    //   accent: colors.indigo.base // #3F51B5
+    // }
+  }
+}
+const VuetifyObj = new Vuetify({
+  lang: {
+    locales: { en, ko },
+    current: 'ko'
+  },
+  theme
+})
+
+export default VuetifyObj
